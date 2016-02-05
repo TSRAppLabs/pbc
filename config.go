1856d7f5a2797073f270d462e8dce26a8b80460b
Max Felgenhauer <felgenh3@gmail.com>
2016-02-05 11:45:16 -0600

    Removed viper from the project

--- config.go
+++ config.go
@@ -1,14 +1,5 @@
 package pbc
 
-import (
-	"github.com/spf13/viper"
-	"os"
-)
-
 func getDataDir() string {
-	return os.ExpandEnv(viper.GetString("core.datadir"))
-}
-
-func init() {
-	viper.SetDefault("core.datadir", os.ExpandEnv("${HOME}/.pbc"))
+	return "/var/pbc/"
 }
